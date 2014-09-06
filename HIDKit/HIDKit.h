afbb80c13ef47519e9e344bba488c32a489143e5
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-06 08:17:01 -0600

    Initial Commit

---
+++ HIDKit/HIDKit.h
@@ -0,0 +1,19 @@
+//
+//  HIDKit.h
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/6/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HIDKit.
+FOUNDATION_EXPORT double HIDKitVersionNumber;
+
+//! Project version string for HIDKit.
+FOUNDATION_EXPORT const unsigned char HIDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HIDKit/PublicHeader.h>
+
+
