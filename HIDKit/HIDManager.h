cbf971df70a6840d98b643b69a27b57fe7dbbb90
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-06 09:15:47 -0600

    Initial implementation.

---
+++ HIDKit/HIDManager.h
@@ -0,0 +1,40 @@
+//
+//  HIDManager.h
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/6/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+	HIDManager is an Objective-C wrapper around OS X's IOHIDManager.
+ */
+@interface HIDManager : NSObject
+
+/**
+	Returns the shared HIDManager instance.
+	
+	@returns The singleton HIDManager instance.
+ */
++ (instancetype)sharedManager;
+
+/**
+	Returns all HID devices found by the system.
+ 
+	@returns An NSArray containing the devices found, if any.
+ */
++ (NSArray *)devices;
+
+/**
+	Returns all HID devices matching the specified criteria, if any. Please refer
+	to the criteria in I/O Kit's HID Manager headers.
+ 
+	@param criteria An NSDictionary containing device matching criteria.
+	
+	@returns An NSArray containing the devices found, if any.
+ */
++ (NSArray *)devicesMatchingDictionary:(NSDictionary *)criteria;
+
+@end
---
+++ HIDKit/HIDManager.m
@@ -0,0 +1,119 @@
+//
+//  HIDManager.m
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/6/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HIDManager.h"
+@import IOKit.hid;
+
+
+// Notifications
+NSString * const HIDManagerDeviceConnectedNotification = @"HIDManagerDeviceConnected";
+NSString * const HIDManagerDeviceRemovedNotification = @"HIDManagerDeviceRemoved";
+
+
+// Device Callbacks
+static void HIDManagerDeviceMatchCallback(void * context, IOReturn result, void * sender, IOHIDDeviceRef device)
+{
+	[[NSNotificationCenter defaultCenter] postNotificationName:HIDManagerDeviceConnectedNotification object:(__bridge id)context];
+}
+
+static void HIDManagerDeviceRemovedCallback(void * context, IOReturn result, void * sender, IOHIDDeviceRef device)
+{
+	[[NSNotificationCenter defaultCenter] postNotificationName:HIDManagerDeviceRemovedNotification object:(__bridge id)context];
+}
+
+
+// Class Extension
+@interface HIDManager ()
+
+@property IOHIDManagerRef manager;
+
+@end
+
+
+// Implementation
+@implementation HIDManager
+
++ (instancetype)sharedManager
+{
+	static HIDManager *sharedInstance;
+	static dispatch_once_t onceToken;
+	
+	dispatch_once(&onceToken, ^{
+		sharedInstance = [[[self class] alloc] init];
+	});
+	
+	return sharedInstance;
+
+}
+
+- (instancetype)init
+{
+	self = [super init];
+	if (self)
+	{
+		_manager = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
+		if (!_manager || CFGetTypeID(_manager) != IOHIDManagerGetTypeID())
+		{
+			return nil;
+		}
+		
+		IOHIDManagerRegisterDeviceMatchingCallback(_manager, &HIDManagerDeviceMatchCallback, (__bridge void *)self);
+		IOHIDManagerRegisterDeviceRemovalCallback(_manager, &HIDManagerDeviceRemovedCallback, (__bridge void *)self);
+		IOHIDManagerScheduleWithRunLoop(_manager, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+		
+		if (IOHIDManagerOpen(_manager, kIOHIDOptionsTypeNone) != kIOReturnSuccess)
+		{
+			IOHIDManagerUnscheduleFromRunLoop(_manager, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+			return nil;
+		}
+	}
+	return self;
+}
+
+- (void)dealloc
+{
+	if (_manager)
+	{
+		IOHIDManagerClose(_manager, kIOHIDOptionsTypeNone);
+		IOHIDManagerUnscheduleFromRunLoop(_manager, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+		CFRelease(_manager);
+	}
+}
+
++ (NSArray *)devices
+{
+	// FIXME: Crashes here when you reset the controller via the reset button.
+	NSSet *rawDevices = CFBridgingRelease(IOHIDManagerCopyDevices([HIDManager sharedManager].manager));
+	
+	NSMutableArray *devices = [NSMutableArray new];
+	for (id deviceRef in rawDevices)
+	{
+		//		DS4Device *device = [DS4Device deviceWithDeviceRef:(__bridge IOHIDDeviceRef)deviceRef];
+		//		[devices addObject:device];
+	}
+	
+	return [devices copy];
+}
+
++ (NSArray *)devicesMatchingDictionary:(NSDictionary *)criteria
+{
+	NSMutableArray *devices = [[[self class] devices] mutableCopy];
+	return [devices copy];
+}
+
+//- (void)setDeviceMatchingCriteria:(IOHIDManagerRef)manager
+//{
+//	NSMutableDictionary *matchingDict = [[NSMutableDictionary alloc] init];
+//	matchingDict[@kIOHIDProductKey] = @"Wireless Controller";
+//	matchingDict[@kIOHIDManufacturerKey] = @"Sony Computer Entertainment";
+//
+//	IOHIDManagerSetDeviceMatching(manager, (__bridge CFDictionaryRef)matchingDict);
+//}
+
+
+@end
