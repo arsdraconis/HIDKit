c5198ec82351603f06e21fe7ffee580bab89fcc4
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-08 02:38:46 -0600

    Added documentation.

--- HIDKit/HIDDevice.h
+++ HIDKit/HIDDevice.h
@@ -9,13 +9,37 @@
 #import <Foundation/Foundation.h>
 @import IOKit.hid;
 
+/**
+	HIDDevice wraps a single IOHIDDeviceRef and provides high level functions for
+	interacting with the device and its properties.
+ */
 @interface HIDDevice : NSObject
 
-@property (readonly) io_service_t service;
-@property (readonly) NSArray *elements;
-- (NSArray *)elementsMatchingDictionary:(NSDictionary *)criteria;
-
-
+/**
+	Designated initialzer for HIDDevice. HIDDevice throw an exception if passed
+	a NULL deviceRef or initialized with init. Initialization will fail if passed
+	a CFTypeRef that is not an IOHIDDeviceRef.
+ 
+	@param deviceRef A valid IOHIDDeviceRef provided by the HIDManager.
+ 
+	@returns An initialized HIDDevice object.
+ */
 - (instancetype)initWithDeviceRef:(IOHIDDeviceRef)deviceRef;
+
+/// Allows access to the device's io_service_t service.
+@property (readonly) io_service_t service;
+
+/// Returns all elements on the device.
+@property (readonly) NSArray *elements;
+
+/**
+	Returns an array of elements matching the criteria specified in a NSDictionary.
+ 
+	@param criteria A NSDictionary containing element matching criteria.
+ 
+	@returns A NSArray of all matching elements. If no matching elements are found,
+			 then this will return an empty array.
+ */
+- (NSArray *)elementsMatchingDictionary:(NSDictionary *)criteria;
 
 @end
