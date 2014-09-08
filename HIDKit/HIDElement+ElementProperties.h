4025fc17ff054b3e735358878073d561dc3503c3
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-08 03:37:34 -0600

    Files added.

---
+++ HIDKit/HIDElement+ElementProperties.h
@@ -0,0 +1,13 @@
+//
+//  HIDElement+ElementProperties.h
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/8/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HIDElement.h"
+
+@interface HIDElement (ElementProperties)
+
+@end
---
+++ HIDKit/HIDElement+ElementProperties.m
@@ -0,0 +1,13 @@
+//
+//  HIDElement+ElementProperties.m
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/8/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HIDElement+ElementProperties.h"
+
+@implementation HIDElement (ElementProperties)
+
+@end
