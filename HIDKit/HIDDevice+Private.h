4df8b6ffb34126ea470d10dc72e507e7a18bc714
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-08 02:09:36 -0600

    Files added.

---
+++ HIDKit/HIDDevice+Private.h
@@ -0,0 +1,18 @@
+//
+//  HIDDevice+Private.h
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/8/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HIDDevice.h"
+
+
+// Private class extension for HIDDevice
+@interface HIDDevice ()
+
+@property IOHIDDeviceRef device;
+
+
+@end
