00301fbd6d899717f9a1d44a3e530d4c0b36a3ee
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-08 02:06:19 -0600

    Files added.

---
+++ HIDKit/HIDElement.h
@@ -0,0 +1,14 @@
+//
+//  HIDElement.h
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/8/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import IOKit.hid;
+
+@interface HIDElement : NSObject
+
+@end
---
+++ HIDKit/HIDElement.m
@@ -0,0 +1,13 @@
+//
+//  HIDElement.m
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/8/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HIDElement.h"
+
+@implementation HIDElement
+
+@end
