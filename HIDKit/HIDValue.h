b3d585575e4f75afedfb85f31976df10d35ab330
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-08 04:44:54 -0600

    Files added.

---
+++ HIDKit/HIDValue.h
@@ -0,0 +1,13 @@
+//
+//  HIDValue.h
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/8/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HIDValue : NSObject
+
+@end
---
+++ HIDKit/HIDValue.m
@@ -0,0 +1,13 @@
+//
+//  HIDValue.m
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/8/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HIDValue.h"
+
+@implementation HIDValue
+
+@end
