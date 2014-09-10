2a5808ee07cbc38b5857709c43e6b354562065d9
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-10 05:09:21 -0600

    Files added.

---
+++ HIDKit/HIDTransaction.m
@@ -0,0 +1,13 @@
+//
+//  HIDTransaction.m
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/10/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HIDTransaction.h"
+
+@implementation HIDTransaction
+
+@end
