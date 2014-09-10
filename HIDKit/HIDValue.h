a2453fde279b2e903e4c2ca77cb42daa1006643a
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-10 03:49:08 -0600

    Basic implementation complete.

--- HIDKit/HIDValue.h
+++ HIDKit/HIDValue.h
@@ -7,7 +7,26 @@
 //
 
 #import <Foundation/Foundation.h>
+@import IOKit.hid;
+
+@class HIDElement;
 
 @interface HIDValue : NSObject
+
+- (instancetype)initWithData:(NSData *)data element:(HIDElement *)element;
+- (instancetype)initWithBytes:(const uint8_t *)bytes length:(NSUInteger)length element:(HIDElement *)element;
+- (instancetype)initWithBytesNoCopy:(const uint8_t *)bytes length:(NSUInteger)length element:(HIDElement *)element;
+- (instancetype)initWithInteger:(NSInteger)value element:(HIDElement *)element;
+
+@property (readonly) HIDElement *element;
+
+@property (readonly, nonatomic) const void * bytes;
+@property (readonly, nonatomic) NSUInteger length;
+
+@property (readonly, nonatomic) uint64_t timeStamp;
+
+@property (readonly, nonatomic) CGFloat scaledPhysicalValue;
+@property (readonly, nonatomic) CGFloat scaledCalibratedValue;
+
 
 @end
--- HIDKit/HIDValue.m
+++ HIDKit/HIDValue.m
@@ -7,7 +7,124 @@
 //
 
 #import "HIDValue.h"
+#import "HIDElement+Private.h"
+@import IOKit.hid;
+
+@interface HIDValue ()
+
+@property (readonly) IOHIDValueRef value;
+
+@end
 
 @implementation HIDValue
+
+//------------------------------------------------------------------------------
+#pragma mark Creation and Destruction
+//------------------------------------------------------------------------------
+- (instancetype)init
+{
+	return [self initWithInteger:0 element:nil];
+}
+
+- (instancetype)initWithData:(NSData *)data element:(HIDElement *)element
+{
+	return [self initWithBytes:data.bytes length:data.length element:element];
+}
+
+- (instancetype)initWithBytes:(const uint8_t *)bytes length:(NSUInteger)length element:(HIDElement *)element
+{
+	return [self initWithBytes:bytes length:length copy:YES element:element];
+}
+
+- (instancetype)initWithBytesNoCopy:(const uint8_t *)bytes length:(NSUInteger)length element:(HIDElement *)element
+{
+	return [self initWithBytes:bytes length:length copy:NO element:element];
+}
+
+- (instancetype)initWithBytes:(const uint8_t *)bytes length:(NSUInteger)length copy:(BOOL)copyFlag element:(HIDElement *)element
+{
+	self = [super init];
+	if (self)
+	{
+		NSParameterAssert(bytes);
+		NSParameterAssert(length);
+		NSParameterAssert(element);
+		
+		_element = element;
+		uint64_t timestamp = mach_absolute_time();
+		
+		if (copyFlag)
+		{
+			_value = IOHIDValueCreateWithBytes(NULL, element.element, timestamp, bytes, (CFIndex)length);
+		}
+		else
+		{
+			_value = IOHIDValueCreateWithBytesNoCopy(NULL, element.element, timestamp, bytes, (CFIndex)length);
+		}
+		
+		if (!_value || CFGetTypeID(_value) != IOHIDValueGetTypeID())
+		{
+			return nil;
+		}
+	}
+	return self;
+}
+
+- (instancetype)initWithInteger:(NSInteger)value element:(HIDElement *)element
+{
+	self = [super init];
+	if (self)
+	{
+		NSParameterAssert(element);
+		
+		_element = element;
+		uint64_t timestamp = mach_absolute_time();
+		
+		_value = IOHIDValueCreateWithIntegerValue(NULL, element.element, timestamp, (CFIndex)value);
+	}
+	return self;
+}
+
+- (void)dealloc
+{
+	if (_value)
+	{
+		CFRelease(_value);
+	}
+}
+
+
+//------------------------------------------------------------------------------
+#pragma mark Properties
+//------------------------------------------------------------------------------
+@dynamic bytes;
+- (const void *)bytes
+{
+	return IOHIDValueGetBytePtr(_value);
+}
+
+@dynamic length;
+- (NSUInteger)length
+{
+	return (NSUInteger)IOHIDValueGetLength(_value);
+}
+
+@dynamic timeStamp;
+- (uint64_t)timeStamp
+{
+	return IOHIDValueGetTimeStamp(_value);
+}
+
+@dynamic scaledPhysicalValue;
+- (CGFloat)scaledPhysicalValue
+{
+	return (CGFloat)IOHIDValueGetScaledValue(_value, kIOHIDValueScaleTypePhysical);
+}
+
+@dynamic scaledCalibratedValue;
+- (CGFloat)scaledCalibratedValue
+{
+	return (CGFloat)IOHIDValueGetScaledValue(_value, kIOHIDValueScaleTypeCalibrated);
+}
 
 @end
