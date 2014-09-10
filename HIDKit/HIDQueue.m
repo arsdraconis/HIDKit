db7ddde2d6a564951b39a40ad784930a1f1522d1
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-10 04:48:04 -0600

    Basic implementation complete.

--- HIDKit/HIDQueue.m
+++ HIDKit/HIDQueue.m
@@ -8,6 +8,133 @@
 
 #import "HIDQueue.h"
 
+// We sure do love poking around inside our friends!
+#import "HIDDevice+Private.h"
+#import "HIDElement+Private.h"
+#import "HIDValue+Private.h"
+
+@import IOKit.hid;
+
+
+
+@interface HIDQueue ()
+
+@property (readonly) IOHIDQueueRef queue;
+
+@end
+
+
+
 @implementation HIDQueue
+
+//------------------------------------------------------------------------------
+#pragma mark Creation and Destruction
+//------------------------------------------------------------------------------
+- (instancetype)init
+{
+	return [self initWithDevice:nil];
+}
+
+- (instancetype)initWithDevice:(HIDDevice *)device
+{
+	return [self initWithDevice:device depth:255];
+}
+
+- (instancetype)initWithDevice:(HIDDevice *)device depth:(NSUInteger)depth
+{
+	self = [super init];
+	if (self)
+	{
+		NSParameterAssert(device);
+		NSParameterAssert(depth);
+		
+		_device = device;
+		_queue = IOHIDQueueCreate(NULL, device.device, depth, kIOHIDOptionsTypeNone);
+		if (!_queue || CFGetTypeID(_queue) != IOHIDQueueGetTypeID() )
+		{
+			return nil;
+		}
+		
+		// TODO: Register the callback and implement a blocks-based handler API.
+		
+		IOHIDQueueScheduleWithRunLoop(_queue, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+	}
+	return self;
+}
+
+- (void)dealloc
+{
+	if (_queue)
+	{
+		// FIXME: Can we pass something that's already been unscheduled?
+		IOHIDQueueUnscheduleFromRunLoop(_queue, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+		CFRelease(_queue);
+	}
+}
+
+//------------------------------------------------------------------------------
+#pragma mark Starting and Stopping Queues
+//------------------------------------------------------------------------------
+- (void)start
+{
+	IOHIDQueueStart(_queue);
+}
+
+- (void)stop
+{
+	IOHIDQueueStop(_queue);
+}
+
+
+//------------------------------------------------------------------------------
+#pragma mark Dequeueing Values
+//------------------------------------------------------------------------------
+- (HIDValue *)nextValue
+{
+	// TODO: Implement me!
+	NSLog(@"Method unimplemented: %s in %s, line %d.", __PRETTY_FUNCTION__, __FILE__, __LINE__);
+	return nil;
+}
+
+- (HIDValue *)nextValueWithTimeout:(CFTimeInterval)timeout
+{
+	// TODO: Implement me!
+	NSLog(@"Method unimplemented: %s in %s, line %d.", __PRETTY_FUNCTION__, __FILE__, __LINE__);
+	return nil;
+}
+
+//------------------------------------------------------------------------------
+#pragma mark Managing Elements
+//------------------------------------------------------------------------------
+// TODO: This could be better expressed with an array property of attached elements.
+// We could hook into object addition/removal to do this.
+- (void)addElement:(HIDElement *)element
+{
+	IOHIDQueueAddElement(_queue, element.element);
+}
+
+- (void)removeElement:(HIDElement *)element
+{
+	IOHIDQueueRemoveElement(_queue, element.element);
+}
+
+- (BOOL)containsElement:(HIDElement *)element
+{
+	return (BOOL)IOHIDQueueContainsElement(_queue, element.element);
+}
+
+//------------------------------------------------------------------------------
+#pragma mark Queue Properties
+//------------------------------------------------------------------------------
+@dynamic depth;
+- (void)setDepth:(NSUInteger)depth
+{
+	IOHIDQueueSetDepth(_queue, (CFIndex)depth);
+}
+
+- (NSUInteger)depth
+{
+	return (NSUInteger)IOHIDQueueGetDepth(_queue);
+}
 
 @end
