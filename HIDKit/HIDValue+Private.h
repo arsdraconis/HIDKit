2e051adc6248266b9822d706bc41c29db3b36b19
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-10 04:46:26 -0600

    File added.

---
+++ HIDKit/HIDValue+Private.h
@@ -0,0 +1,13 @@
+//
+//  HIDValue+Private.h
+//  HIDKit
+//
+//  Created by Robert Luis Hoover on 9/10/14.
+//  Copyright (c) 2014 ars draconis. All rights reserved.
+//
+
+#import "HIDValue.h"
+
+@interface HIDValue (Private)
+
+@end
