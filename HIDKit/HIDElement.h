d0c555f584c2d348e9fabee8a9835addb0e48138
Robert Luis Hoover <shiroholloway@gmail.com>
2014-09-10 04:07:04 -0600

    Added documentation.

--- HIDKit/HIDElement.h
+++ HIDKit/HIDElement.h
@@ -13,12 +13,27 @@
 
 @interface HIDElement : NSObject
 
+/**
+	Designated initialzer for HIDElement. HIDElement throw an exception if passed
+	a NULL element or initialized with init. Initialization will fail if passed
+	a CFTypeRef that is not an IOHIDElementRef.
+ 
+	@param element A valid IOHIDElementRef.
+	@param device The HIDDevice representing the device this element belong to.
+	@param parentElement The parent element of this element.
+ 
+	@returns An initialized HIDElement object, or nil if it could not be created.
+ */
 - (instancetype)initWithElementRef:(IOHIDElementRef)element onDevice:(HIDDevice *)device parent:(HIDElement *)parentElement NS_DESIGNATED_INITIALIZER;
 
+/// The device to which this element belongs to.
 @property (readonly) HIDDevice *parentDevice;
+/// The parent element of this element.
 @property (readonly) HIDElement *parent;
+/// The children elements of this element. If none, this returns an empty array.
 @property (readonly) NSArray *children;
 
+/// The element type of this element.
 @property (readonly) IOHIDElementType type;
 
 @end
